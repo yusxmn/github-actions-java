c8a4eff7679eb5441e3361a9fa6327102a834343
유성민 <dbtjdals21@naver.com>
2023-11-25 20:24:59 +0900

    Update Fibonacci.java

--- src/main/java/kr/ac/hansung/Fibonacci.java
+++ src/main/java/kr/ac/hansung/Fibonacci.java
@@ -14,7 +14,10 @@ public class Fibonacci {
         }
 
         for (int i = 0; i < n; i++) {
-            System.out.print(fibonacci(i) + " ");
+            // System.out.print(fibonacci(i) + " ");
+            int fib = fibonacci(i);
+            System.out.print("fibonacci(" + i + ")=" + fib);
+            System.out.println();
         }
     }
 
